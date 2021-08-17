9c61a42233c0d2e674df50b23fdd00a15c6cd5ce
Marinac <nikola@regex-sh.com>
2021-08-17 16:17:34 +0200

    Setup basic auth for admin dashboard

--- lib/iparty_backend_web/router.ex
+++ lib/iparty_backend_web/router.ex
@@ -1,5 +1,10 @@
 defmodule IpartyBackendWeb.Router do
   use IpartyBackendWeb, :router
+  import Plug.BasicAuth
+
+  pipeline :admin do
+    plug :basic_auth, Application.compile_env(:iparty_backend, :admin_dashboard)
+  end
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -9,19 +14,10 @@ defmodule IpartyBackendWeb.Router do
     pipe_through :api
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: IpartyBackendWeb.Telemetry
-    end
+  scope "/admin", IpartyBackendWeb do
+    pipe_through [:fetch_session, :protect_from_forgery, :admin]
+    live_dashboard "/dashboard", metrics: IpartyBackendWeb.Telemetry
   end
 end
